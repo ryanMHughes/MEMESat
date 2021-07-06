a22558a3358eb094a523f19fb95cfbad2f8efdac
ryanMHughes <71276738+ryanMHughes@users.noreply.github.com>
2021-07-05 21:40:29 -0400

    Create GS_VHF.m
    Generating radiation pattern and antenna model for GS Yagi-Uda (VHF downlink).

---
+++ GS_VHF.m
@@ -0,0 +1,34 @@
+% Ground Station antenna radiation pattern (UHF Cross 
+% Polarized Yagi-Uda configuration)
+
+%Creating Yagi Objects
+yagiAntenna1 = yagiUda;
+yagiAntenna2 = yagiUda;
+
+%Defining properties of both yagi objects
+yagiAntenna1.Exciter = dipoleFolded;
+yagiAntenna2.Exciter = dipoleFolded;
+
+yagiAntenna1.Exciter.Width = 
+yagiAntenna2.Exciter.Width =
+
+yagiAntenna1.Exciter.Spacing = 
+yagiAntenna2.Exciter.Spacing =
+
+yagiAntenna1.Exciter.Length = 
+yagiAntenna2.Exciter.Length =
+
+yagiAntenna1.NumDirectors = 
+yagiAntenna2.NumDirectors =
+
+yagiAntenna1.DirectorLength = 
+yagiAntenna2.DirectorLength =
+
+yagiAntenna1.DirectorSpacing = 
+yagiAntenna2.DirectorSpacing =
+
+yagiAntenna1.ReflectorLength = 
+yagiAntenna2.ReflectorLength =
+
+yagiAntenna1.ReflectorSpacing = 
+yagiAntenna2.ReflectorSpacing =
